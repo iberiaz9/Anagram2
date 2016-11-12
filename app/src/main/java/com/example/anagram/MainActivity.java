cfe837901a452d797573fb7a74aa72eaab7f805a
Jose Alvarado <alvarado.ja53@gmail.com>
2016-11-11 18:34:43 -0800

    Replaced tabs with spaces.

--- app/src/main/java/com/example/anagram/MainActivity.java
+++ app/src/main/java/com/example/anagram/MainActivity.java
@@ -23,15 +23,15 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-	private static final String TAG = "MainActivity";
-	TextView mTextView;
-	EditText mEditText;
-	String mPrevAns = "";
-	String mTheWord;
-	Random random = new Random();
-	String [] lines;
-	int max;
-	AsyncTask<Integer, Void, Integer> mAtask = null;
+    private static final String TAG = "MainActivity";
+    TextView mTextView;
+    EditText mEditText;
+    String mPrevAns = "";
+    String mTheWord;
+    Random random = new Random();
+    String [] lines;
+    int max;
+    AsyncTask<Integer, Void, Integer> mAtask = null;
 
     private class myAtask extends AsyncTask<Integer, Void, Integer> {
         @Override
@@ -52,35 +52,35 @@ public class MainActivity extends Activity {
             mAtask = null;
         }
     }
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		mTextView = (TextView) findViewById(R.id.textView1);
-		mEditText = (EditText) findViewById(R.id.editText1);
-		String line = readRawTextFile(R.raw.anag);
-		lines = line.split("\n");
-		max = lines.length;
-		
-		mTextView.setText("Press Start for anagram puzzle ...");
-		
-		final Button button = (Button) findViewById(R.id.button1);
-		final Button button2 = (Button) findViewById(R.id.button2);
 
-		// Link UI elements to actions in code		
-		button.setOnClickListener(new Button.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				try {
-					int idx = (int) (random.nextFloat() * max);
-					String [] words = lines[idx].split(" ");
-					int wmax = words.length;
-					int widx = (int) (random.nextFloat() * wmax);
-					mTheWord = words[widx];
-					mTextView.setText(wmax - 1 + " anagram(s) of " + mTheWord);
-					mEditText.setText("");
-					mPrevAns = lines[idx];
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.textView1);
+        mEditText = (EditText) findViewById(R.id.editText1);
+        String line = readRawTextFile(R.raw.anag);
+        lines = line.split("\n");
+        max = lines.length;
+
+        mTextView.setText("Press Start for anagram puzzle ...");
+
+        final Button button = (Button) findViewById(R.id.button1);
+        final Button button2 = (Button) findViewById(R.id.button2);
+
+        // Link UI elements to actions in code
+        button.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    int idx = (int) (random.nextFloat() * max);
+                    String [] words = lines[idx].split(" ");
+                    int wmax = words.length;
+                    int widx = (int) (random.nextFloat() * wmax);
+                    mTheWord = words[widx];
+                    mTextView.setText(wmax - 1 + " anagram(s) of " + mTheWord);
+                    mEditText.setText("");
+                    mPrevAns = lines[idx];
                     if (mAtask != null) mAtask.cancel(true);
                     SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
                     String toval = prefs.getString(getString(R.string.pref_location_key),
@@ -92,48 +92,48 @@ public class MainActivity extends Activity {
                             mAtask.execute(val);
                         }
                     }
-				} catch (Exception e) {
-					Log.e(TAG, e.toString());
-				}
-			}
-		});
-		
-		button2.setOnClickListener(new Button.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				try {
-					String ans = mEditText.getText().toString().toLowerCase();
-					if ( !ans.isEmpty() )
-					    ans = mTheWord + " " + ans;
-					String [] words = mPrevAns.split(" ");
-					int j = 0;
-					if ( !ans.isEmpty() ) {
-						for (int i = 0; i < words.length ; i++)
-						    if (ans.contains(words[i]))
-						        j++;
-						if ( j == words.length )
-							mTextView.setText("Correct! :-)");
-						else
-							mTextView.setText("Sorry :-(");
-					} else
-						mTextView.setText("Sorry :-(");
+                } catch (Exception e) {
+                    Log.e(TAG, e.toString());
+                }
+            }
+        });
 
-					mEditText.setText(mPrevAns);
+        button2.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String ans = mEditText.getText().toString().toLowerCase();
+                    if ( !ans.isEmpty() )
+                        ans = mTheWord + " " + ans;
+                    String [] words = mPrevAns.split(" ");
+                    int j = 0;
+                    if ( !ans.isEmpty() ) {
+                        for (int i = 0; i < words.length ; i++)
+                            if (ans.contains(words[i]))
+                                j++;
+                        if ( j == words.length )
+                            mTextView.setText("Correct! :-)");
+                        else
+                            mTextView.setText("Sorry :-(");
+                    } else
+                        mTextView.setText("Sorry :-(");
+
+                    mEditText.setText(mPrevAns);
                     if (mAtask != null) mAtask.cancel(true);
                     mAtask = null;
-				} catch (Exception e) {
-					Log.e(TAG, e.toString());
-				}
-			}
-		});
-	}
+                } catch (Exception e) {
+                    Log.e(TAG, e.toString());
+                }
+            }
+        });
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -150,25 +150,25 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
-	
-	private String readRawTextFile(int resId)
-	{
-	    InputStream inputStream = this.getResources().openRawResource(resId);
 
-	    InputStreamReader inputreader = new InputStreamReader(inputStream);
-	    BufferedReader buffreader = new BufferedReader(inputreader);
-	    String line;
-	    StringBuilder text = new StringBuilder();
+    private String readRawTextFile(int resId)
+    {
+        InputStream inputStream = this.getResources().openRawResource(resId);
 
-	    try {
-	        while (( line = buffreader.readLine()) != null) {
-	            text.append(line);
-	            text.append('\n');
-	        }
-	    } catch (IOException e) {
-	        return null;
-	    }
-	    
-	    return text.toString();
-	}
+        InputStreamReader inputreader = new InputStreamReader(inputStream);
+        BufferedReader buffreader = new BufferedReader(inputreader);
+        String line;
+        StringBuilder text = new StringBuilder();
+
+        try {
+            while (( line = buffreader.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+            }
+        } catch (IOException e) {
+            return null;
+        }
+
+        return text.toString();
+    }
 }
